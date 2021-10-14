6ed4d3338c42ac09986ad96d29d132790b45d324
James <92519535+JaaaaamesDEV@users.noreply.github.com>
2021-10-14 12:56:48 +0200

    Initial commit

---
+++ src/main/java/de/jamesdev/repotest/main/Main.java
@@ -0,0 +1,8 @@
+package de.jamesdev.repotest.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello this is a test"); //message if the program starts
+    }
+}
